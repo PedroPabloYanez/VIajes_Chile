8eb18dbe2ad356b2e28c5d9db3866d17b8d950ac
pedro pablo <pedropablo.yanez@hotmail.com>
2021-12-08 15:04:30 -0300

    Add change title card with JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
+    $('[data-bs-toggle="tooltip"]').tooltip();
     $(".scroll").click(function(){
         var gato = this.hash
         $("html, body").animate({
             scrollTop: $(gato).offset().top - 70
         },800)
     })
-    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+    $(".card-title").on("dblclick change", function(){
+        $(this).css({
+            "color":"red"
+        })
+    });
 })
